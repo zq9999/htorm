d8b4b00beab94a93ac941f95fd578614bdc1e34c
zq9999 <806153045@qq.com>
2020-11-27 18:04:48 +0800

    1

--- HT.WebApi/Startup.cs
+++ HT.WebApi/Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using HT.WebApi.Utility;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace HT.WebApi
 {
@@ -29,6 +32,27 @@ namespace HT.WebApi
         {
             services.AddControllers();
             //services.AddScoped<IUserService, UserService>();
+
+            #region jwt校验
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,//是否验证Issuer
+                    ValidateAudience = true,//是否验证Audience
+                    ValidateLifetime = true,//是否验证失效时间
+                    ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                    ValidAudience = this.Configuration["audience"],//Audience
+                    ValidIssuer = this.Configuration["issuer"],//Issuer，这两项和前面签发jwt的设置一致
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.Configuration["SecurityKey"])),//拿到SecurityKey
+                    //AudienceValidator = (m, n, z) =>
+                    //{
+                    //    return m != null && m.FirstOrDefault().Equals(this.Configuration["audience"]);
+                    //},//自定义校验规则，可以新登录后将之前的无效
+                };
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +64,10 @@ namespace HT.WebApi
             }
 
             app.UseHttpsRedirection();
+
+            #region jwt
+            app.UseAuthentication();//注意添加这一句，启用验证
+            #endregion
 
             app.UseRouting();
 
