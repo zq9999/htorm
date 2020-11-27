72a514ba4f7b1035b15d1d446d304f328e662b60
zq9999 <806153045@qq.com>
2020-11-27 08:26:48 +0800

    1

---
+++ HT.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HT.WebApi.Controllers
+{
+   
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route("api/Health/Index")]
+        public ActionResult Index() {
+            return Ok();
+        }
+    }
+}
