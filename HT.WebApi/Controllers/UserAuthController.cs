95fa210ece6bad0a3f4440dbcf893ef9d7b7a3a7
zq9999 <806153045@qq.com>
2020-12-02 08:56:36 +0800

    1

--- HT.WebApi/Controllers/UserAuthController.cs
+++ HT.WebApi/Controllers/UserAuthController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,12 +14,26 @@ namespace HT.WebApi.Controllers
 
     public class UserAuthController : ControllerBase
     {
-        [Route("GetUsers")]
-        
+        public UserAuthController()
+        {
+            int a = 1;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")] 
         public List<string> GetUsers() {
             base.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");//允许跨域
+
+            var nickName = HttpContext.AuthenticateAsync().Result.Principal.Claims.FirstOrDefault(c => c.Type.Equals("NickName"))?.Value;
+            Console.WriteLine($"This is GetUserByName 校验 {nickName}");
+
+            var role = HttpContext.AuthenticateAsync().Result.Principal.Claims.FirstOrDefault(c => c.Type.Equals("name"))?.Value;
+            Console.WriteLine($"This is role 校验 {role}"); 
+
             return new List<string>() { "jack","jim","wade"};
         }
+
+        [HttpGet]
         [Route("GetUsersByName")]
         [Microsoft.AspNetCore.Authorization.AllowAnonymous]
         public dynamic  GetUsersByName(string name)
